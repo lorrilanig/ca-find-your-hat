d658b4f2005ff0854f72370335e117bcc7ce1ed7
Lorri Lanig <lorrilanig.art@gmail.com>
2024-11-22 12:03:18 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+//test run with node main.js
+
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
