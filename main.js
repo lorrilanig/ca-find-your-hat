beefdb9f54c5421ff8c24a76f7f39e15703f891b
Lorri Lanig <lorrilanig.art@gmail.com>
2024-11-22 12:46:35 -0600

    print method working

--- main.js
+++ main.js
@@ -6,3 +6,31 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field){
+        this.field = [field];
+    }
+    //method prints out field in 'board' format
+    print(){
+        for (let i = 0; i < this.field.length; i++) {
+            this.field[i].forEach(arr => {
+                arr = arr.join("");
+                console.log(arr);
+            })
+        }}
+    //method to handle user input and update location
+
+    //method to test current location, win, hole or out-of-bounds
+
+    //method to run the main game until game is won or lost
+
+    
+}
+const myField = new Field([
+    [pathCharacter, fieldCharacter, hole],
+    [fieldCharacter, hole, fieldCharacter],
+    [fieldCharacter, hat, fieldCharacter]
+])
+
+myField.print()
