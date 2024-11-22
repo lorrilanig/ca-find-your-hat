0fa20072715de7c7cde96d9d0b3caa958fb50369
Lorri Lanig <lorrilanig.art@gmail.com>
2024-11-22 14:06:46 -0600

    user input updates field

--- main.js
+++ main.js
@@ -8,29 +8,64 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-    constructor(field){
-        this.field = [field];
+    constructor(field = [[]]){
+        this.field = field;
+        this.row = 0;
+        this.col = 0;
+        //home location 
+        this.field[0][0] = pathCharacter;
+    }
+
+    runGame(){
+        let playing = true;
+        while(playing) {
+            this.print();
+            this.handleInput();
+        
+        //update location
+        this.field[this.col][this.row] = pathCharacter;
+        }
     }
     //method prints out field in 'board' format
     print(){
-        for (let i = 0; i < this.field.length; i++) {
-            this.field[i].forEach(arr => {
-                arr = arr.join("");
-                console.log(arr);
-            })
-        }}
+        const display = this.field.map(row => {
+            return row.join('');
+        }).join('\n');
+        console.log(display);
+        }
     //method to handle user input and update location
-
+        handleInput(){
+            const answer = prompt('Which way? ').toUpperCase();
+            if (answer == 'U') {
+                this.col -= 1;
+            } else if (answer == 'D'){
+                this.col += 1;
+            } else if (answer == 'R') {
+                this.row += 1
+            } else if (answer == 'L') {
+                this.row -= 1;
+            } else {
+                console.log('Enter U, D, L, or R.')
+                this.handleInput();
+            }
+        }
     //method to test current location, win, hole or out-of-bounds
+        inBounds(){
 
-    //method to run the main game until game is won or lost
+        }
+        onHat(){
 
-    
+        }
+        onHole(){
+
+        }
 }
-const myField = new Field([
+let myField = new Field([
     [pathCharacter, fieldCharacter, hole],
     [fieldCharacter, hole, fieldCharacter],
     [fieldCharacter, hat, fieldCharacter]
 ])
 
-myField.print()
+//myField.print();
+
+myField.runGame();
